1459cca9934314e7deab06a0531dc183eefc0736
RobinsonZam025 <robinsonzambranos1@hotmail.com>
2023-10-29 13:12:20 -0500

    resgister

--- js/script.js
+++ js/script.js
@@ -1,28 +1,33 @@
 // Funcion de Registro
 let contador = 0;
+let nombresRegistrados = []; // Variable global para almacenar nombres
+
 function agregarNombre() {
     const nombre = document.getElementById("nombre").value;
-        if (nombre) {
-            contador++;
-            const tabla = document.getElementById("tablaNombres");
-            const row = tabla.insertRow(-1);
-            const idCell = row.insertCell(0);
-            const nombreCell = row.insertCell(1);
-            idCell.innerHTML = contador;
-            nombreCell.innerHTML = nombre;
-            document.getElementById("nombre").value = "";
-            actualizarTotalRegistros();
-            guardarDatosEnLocalStorage();
-        }
-}function actualizarTotalRegistros() {
+    if (nombre) {
+        contador++;
+        const tabla = document.getElementById("tablaNombres");
+        const row = tabla.insertRow(-1);
+        const idCell = row.insertCell(0);
+        const nombreCell = row.insertCell(1);
+        idCell.innerHTML = contador;
+        nombreCell.innerHTML = nombre;
+        document.getElementById("nombre").value = "";
+        actualizarTotalRegistros();
+
+        // Agregar el nombre a la variable global
+        nombresRegistrados.push(nombre);
+
+        // Guardar los nombres en el almacenamiento local
+        guardarDatosEnLocalStorage();
+    }
+}
+
+function actualizarTotalRegistros() {
     const totalRegistros = document.getElementById("totalRegistros");
     totalRegistros.innerHTML = contador;
 }
+
 function guardarDatosEnLocalStorage() {
-    const nombres = [];
-        for (let i = 1; i <= contador; i++) {
-                const nombre = document.querySelector(`#tablaNombres tr:nth-child(${i + 1}) td:nth-child(2)`).textContent;
-                nombres.push(nombre);
-        }
-    localStorage.setItem("nombres", JSON.stringify(nombres));
+    localStorage.setItem("nombres", JSON.stringify(nombresRegistrados));
 }
